b1556a2c5f45442f613fc37df0d27b201345368e
kirito-k <devavratk96@gmail.com>
2020-10-14 19:34:44 -0400

    Added image to Sound logic

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function buttonClicked() {
-    alert("He actually clicked me!!");
+let audio_files = ["crash.mp3", "kick-bass.mp3", "snare.mp3", "tom-1.mp3",
+                    "tom-2.mp3", "tom-3.mp3", "tom-4.mp3"];
+
+let button_tag = document.querySelectorAll("Button");
+for (let index = 0; index < button_tag.length; index++) {
+    button_tag[index].addEventListener("click", function() {
+        let path = "./sounds/" + audio_files[index];
+        new Audio(path).play()
+    });
 }
+
