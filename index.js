2c70ef3b1dd5684cf70c6a8ef847b9e3c33dcb65
kirito-k <devavratk96@gmail.com>
2020-10-14 19:35:00 -0400

    Added keystrokes to sound logic

--- index.js
+++ index.js
@@ -9,3 +9,31 @@ for (let index = 0; index < button_tag.length; index++) {
     });
 }
 
+document.addEventListener('keydown', function(event) {
+    let key = event.key;
+    switch(key) {
+        case 'w':
+            new Audio("./sounds/crash.mp3").play();
+            break;
+        case 'a':
+            new Audio("./sounds/kick-bass.mp3").play();
+            break;
+        case 's':
+            new Audio("./sounds/snare.mp3").play();
+            break;
+        case 'd':
+            new Audio("./sounds/tom-1.mp3").play();
+            break;
+        case 'j':
+            new Audio("./sounds/tom-2.mp3").play();
+            break;
+        case 'k':
+            new Audio("./sounds/tom-3.mp3").play();
+            break;
+        case 'l':
+            new Audio("./sounds/tom-4.mp3").play();
+            break;
+        default:
+            break;
+        }
+})
