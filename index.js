d81260eb15715d36f7649f89637d5dfa21e8cb23
kirito-k <devavratk96@gmail.com>
2020-10-14 20:05:42 -0400

    Added button click logic

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ let audio_files = ["crash.mp3", "kick-bass.mp3", "snare.mp3", "tom-1.mp3",
 let button_tag = document.querySelectorAll("Button");
 // Assign audio files to play on button click using event listener
 for (let index = 0; index < button_tag.length; index++) {
+    
     button_tag[index].addEventListener("click", function() {
         let path = "./sounds/" + audio_files[index];
         new Audio(path).play()
+        button_animation(this.innerHTML);
     });
+
 }
 
 // Assign audio files to play on specific key press using event listener
@@ -18,26 +21,43 @@ document.addEventListener('keydown', function(event) {
     switch(key) {
         case 'w':
             new Audio("./sounds/crash.mp3").play();
+            button_animation(key);
             break;
         case 'a':
             new Audio("./sounds/kick-bass.mp3").play();
+            button_animation(key);
             break;
         case 's':
             new Audio("./sounds/snare.mp3").play();
+            button_animation(key);
             break;
         case 'd':
             new Audio("./sounds/tom-1.mp3").play();
+            button_animation(key);
             break;
         case 'j':
             new Audio("./sounds/tom-2.mp3").play();
+            button_animation(key);
             break;
         case 'k':
             new Audio("./sounds/tom-3.mp3").play();
+            button_animation(key);
             break;
         case 'l':
             new Audio("./sounds/tom-4.mp3").play();
+            button_animation(key);
             break;
         default:
             break;
         }
 })
+
+// Add button click animation
+function button_animation(key) {
+    let button_tag = document.querySelector('.' + key);
+    button_tag.classList.add('pressed');
+
+    setTimeout(function() {
+        button_tag.classList.remove('pressed');
+    }, 100);
+}
